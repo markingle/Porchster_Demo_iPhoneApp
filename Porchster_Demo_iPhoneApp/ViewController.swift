38c2b7f248d6d54bfa559155ef2a18fa4d200f26
Mark Ingle <mark.ingle@comporium.com>
2019-11-10 17:40:58 -0500

    Initial Commit

---
+++ Porchster_Demo_iPhoneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Porchster_Demo_iPhoneApp
+//
+//  Created by Mark Brady Ingle on 11/10/19.
+//  Copyright © 2019 Mark Brady Ingle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
